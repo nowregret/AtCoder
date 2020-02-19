d2607484694da72b52cf71969e4e80d06919f1d2
nowregret <48401878+nowregret@users.noreply.github.com>
2020-02-19 10:26:46 +0900

    Update and rename #Beginner Contest A/a001.java to Beginner Contest A/001.java

---
+++ Beginner Contest A/001.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+ 
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+ 
+	Scanner scanner = new Scanner(System.in);
+ 	int a = Integer.parseInt(scanner.next());
+    int b = Integer.parseInt(scanner.next());
+   	System.out.println(a-b);
+  }
+}
