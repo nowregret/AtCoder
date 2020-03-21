29eea1ca90a17b3efae19cc255db695c579a02c3
nowregret <nowregretnow@users.noreply.github.com>
2020-03-22 03:19:41 +0900

    https://atcoder.jp/contests/abc028/submissions/11066097

--- Beginner Contest A/027.java
+++ Beginner Contest A/027.java
@@ -1,25 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.stream.Collectors;
+ 
 class Main {
-
-  public static void main(String[] args) throws Exception {
-
-    try {
-
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-      int input = Integer.parseInt(br.readLine());
-
-      System.out.println((int)Math.pow(input/2, 2));
-
-    } catch(IOException | NumberFormatException e) {
-      e.printStackTrace();
-      System.exit(0);
-    } catch(Exception e) {
-      e.printStackTrace();
-      System.exit(0);
-    }
-  }
+ 
+	public static void main(String[] args) throws Exception {
+ 
+		try {
+ 
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+			String[] inputString = br.readLine().split("\\s+");
+ 
+			Map<String, List<String>> map = Arrays.asList(inputString)
+					.stream()
+					.sorted()
+					.collect(Collectors.groupingBy(s -> s));
+			
+			for(Entry<String, List<String>> resultMap : map.entrySet()) {
+				if(resultMap.getValue().size() == 1 || resultMap.getValue().size() > 2) {
+					System.out.println(resultMap.getKey());
+				}
+			}
+			
+		} catch (IOException | NumberFormatException e) {
+			e.printStackTrace();
+			System.exit(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
 }
---
+++ Beginner Contest A/028.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+class Main {
+ 
+  public static void main(String[] args) throws Exception {
+
+    try {
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int score = Integer.parseInt(br.readLine());
+
+      if (score == 100) {
+        System.out.println("Perfect");
+      } else if (score <= 99) {
+        System.out.println("Great");
+      } else if (score <= 89) {
+        System.out.println("Good");
+      } else if (score <= 59) {
+        System.out.println("Bad");
+      }
+
+    } catch (IOException | NumberFormatException e) {
+      e.printStackTrace();
+      System.exit(0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(0);
+    }
+  }
+}
