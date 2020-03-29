6f1ed206f8e0ceaaae814f18f0d9d3f8be9b7022
nowregret <nowregretnow@gmail.com>
2020-03-29 16:34:10 +0900

    BeginnerContestA057
    https://atcoder.jp/contests/abc057/submissions/11338570

--- Beginner Contest A/057.java
+++ Beginner Contest A/057.java
@@ -1,25 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
- 
+
 class Main {
- 
+
     public static void main(String[] args) throws Exception {
- 
+
         try {
- 
+
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-            String[] peopleSpeaks = br.readLine().split(" ");
-            String atCoDeerSpeak = peopleSpeaks[0];
-            String topCoDeerSpeak = peopleSpeaks[1];
- 
-            if (atCoDeerSpeak.equals(topCoDeerSpeak)) {
-                System.out.println("H");
+
+            String[] times = br.readLine().split(" ");
+            int now = Integer.parseInt(times[0]);
+            int contestStartFromNow = Integer.parseInt(times[1]);
+
+            int contestStratTime = now + contestStartFromNow;
+
+            // convert to 24-hour display
+            if (contestStratTime < 24) {
+                System.out.println(contestStratTime);
             } else {
-                System.out.println("D");
+                System.out.println(contestStratTime - 24);
             }
- 
+
         } catch (IOException | NumberFormatException e) {
             e.printStackTrace();
             System.exit(0);
