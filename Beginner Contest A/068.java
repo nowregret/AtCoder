5fe13bcb4c32e09af5dbb7b7141a81d75a21299b
nowregret <nowregretnow@gmail.com>
2020-03-31 22:21:56 +0900

    BeginnerContestA068
    https://atcoder.jp/contests/abc068/submissions/11396575

---
+++ Beginner Contest A/068.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("ABC" + br.readLine());
+
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
