f69046ce7c2f5805e87d2f7dcdebaab3c84ea600
nowregret <nowregretnow@gmail.com>
2020-04-12 22:19:43 +0900

    BeginnerContest084
    https://atcoder.jp/contests/abc084/submissions/11572269

---
+++ Beginner Contest A/084.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int nowHour = Integer.parseInt(br.readLine());
+            int needToNewYear = 48 - nowHour;
+
+            System.out.println(needToNewYear);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
