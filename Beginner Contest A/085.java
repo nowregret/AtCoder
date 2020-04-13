3c9612116c25b63d30b1cd977f51a586d7677b77
nowregret <nowregretnow@gmail.com>
2020-04-13 22:13:39 +0900

    BeginnerContestA085
    https://atcoder.jp/contests/abc085/submissions/11572475

---
+++ Beginner Contest A/085.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String dateString = br.readLine();
+            String convertTo2018 = "2018" + dateString.substring(4, 10);
+
+            System.out.println(convertTo2018);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
