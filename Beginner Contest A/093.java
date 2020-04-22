94ae40ac204cab4f4cc16f39af7aa70b9ae4bdfc
nowregret <nowregretnow@gmail.com>
2020-04-22 23:21:54 +0900

    BeginnerContestA093
    https://atcoder.jp/contests/abc093/submissions/11931204

---
+++ Beginner Contest A/093.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String input = br.readLine();
+
+            String result = "No";
+            if (input.contains("a") && input.contains("b") && input.contains("c")) {
+                result = "Yes";
+            }
+
+            System.out.println(result);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
