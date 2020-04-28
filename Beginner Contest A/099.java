159858eba5f3ce112e50e8e3647fef439bff3d9f
nowregret <nowregretnow@gmail.com>
2020-04-28 23:03:19 +0900

    BeginnerContestA099
    https://atcoder.jp/contests/abc099/submissions/11578649

---
+++ Beginner Contest A/099.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String result = "ABC";
+            if (Integer.parseInt(br.readLine()) >= 1000) {
+                result = "ABD";
+            }
+
+            System.out.println(result);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
