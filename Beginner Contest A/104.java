a966076696530f005f56026ce1648b695c066591
nowregret <nowregretnow@gmail.com>
2020-05-03 23:18:33 +0900

    BeginnerContestA104
    https://atcoder.jp/contests/abc104/submissions/11579870

---
+++ Beginner Contest A/104.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int rating = Integer.parseInt(br.readLine());
+
+            String result = "ABC";
+            if (rating >= 2800) {
+                result = "AGC";
+            } else if (rating >= 1200) {
+                result = "ARC";
+            }
+
+            System.out.println(result);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
