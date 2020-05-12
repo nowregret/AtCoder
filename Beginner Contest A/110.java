cb7de70eba07743f460cb15093086771a6eccd8f
nowregret <nowregretnow@gmail.com>
2020-05-12 21:09:14 +0900

    BeginnerContestA110
    https://atcoder.jp/contests/abc110/submissions/11619752

---
+++ Beginner Contest A/110.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            List<String> numbers = Arrays.asList(br.readLine().split(" "));
+            numbers = numbers.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+            int result = Integer.parseInt(numbers.get(0) + numbers.get(1)) + Integer.parseInt(numbers.get(2));
+            System.out.println(result);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
