48a3fbbd940a6bf14c0d4eb875fa12581579c8ba
nowregret <nowregretnow@gmail.com>
2020-05-13 21:51:35 +0900

    BeginnerContestA111
    https://atcoder.jp/contests/abc111/submissions/11619928

---
+++ Beginner Contest A/111.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            List<String> numbers = Arrays.asList(br.readLine().split(""));
+            numbers.stream().forEach(s -> System.out.print(s.equals("9") ? "1" : "9"));
+            System.out.println();
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
