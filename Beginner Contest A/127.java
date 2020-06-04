c96467f8115b105d62d48a57f9b0582c2a46bdef
nowregret <nowregretnow@gmail.com>
2020-06-05 07:40:41 +0900

    BeginnerContestA127
    https://atcoder.jp/contests/abc127/submissions/11671797

---
+++ Beginner Contest A/127.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            List<String> input = Arrays.asList(br.readLine().split(" "));
+            int age = Integer.parseInt(input.get(0));
+            int charge = Integer.parseInt(input.get(1));
+
+            int result = charge;
+
+            if (age <= 5) {
+                result = 0;
+            } else if (age <= 12) {
+                result = charge / 2;
+            }
+
+            System.out.println(result);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
