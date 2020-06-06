43c907e0278a2c722651daad39adf51b166c56ea
nowregret <nowregretnow@gmail.com>
2020-06-06 20:39:41 +0900

    BeginnerContestA128
    https://atcoder.jp/contests/abc128/submissions/11671994

---
+++ Beginner Contest A/128.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            List<String> input = Arrays.asList(br.readLine().split(" "));
+            int normalAppleCount = Integer.parseInt(input.get(0));
+            int errorAppleCount = Integer.parseInt(input.get(1));
+
+            int applePieCount = (normalAppleCount * 3 + errorAppleCount) / 2;
+
+            System.out.println(applePieCount);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
