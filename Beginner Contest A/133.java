22f0a39df6632934743ba85a021a7b1ccd9b40e5
nowregret <nowregretnow@gmail.com>
2020-06-11 08:21:38 +0900

    BeginnerContestA133
    https://atcoder.jp/contests/abc133/submissions/11698672

---
+++ Beginner Contest A/133.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            List<String> input = Arrays.asList(br.readLine().split(" "));
+            List<Integer> numbers = new ArrayList<>();
+
+            for (String s : input) {
+                numbers.add(Integer.parseInt(s));
+            }
+
+            int busCharge = numbers.get(0) * numbers.get(1);
+            int taxiCharge = numbers.get(2);
+            int result = taxiCharge;
+            if (busCharge < taxiCharge) {
+                result = busCharge;
+            }
+
+            System.out.println(result);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
