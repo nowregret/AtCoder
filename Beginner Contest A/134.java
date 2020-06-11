3cba6c63d367a20b96431fb58ec318d147afc44d
nowregret <nowregretnow@gmail.com>
2020-06-12 07:56:22 +0900

    BeginnerContestA134
    https://atcoder.jp/contests/abc134/submissions/11698772

---
+++ Beginner Contest A/134.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int input = Integer.parseInt(br.readLine());
+
+            int result = input * input * 3;
+
+            System.out.println(result);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
